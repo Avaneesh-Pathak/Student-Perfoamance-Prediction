9566061c0fac5dbbef0b7da0a534401597fd1003
Avaneesh-Pathak <avaneeshpathak900@gmail.com>
2023-06-03 13:08:04 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+
+HYPHEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    
+    """
+    this function will return list of requirements_
+    """
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        [req.replace ("\n","") for req in requirements]
+        
+        
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+            
+    return requirements
+
+setup(
+    name="MLproject",
+    version='0.0.1',
+    author='Avaneesh',
+    author_email="avaneeshpathak900@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+    
+    
+)
