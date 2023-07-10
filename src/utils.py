0cb15dd3bff17a38c34c882f3eeaeea51e4423f6
Avaneesh-Pathak <avaneeshpathak900@gmail.com>
2023-07-10 22:26:20 +0530

    Model Trainer Done

--- src/utils.py
+++ src/utils.py
@@ -3,16 +3,43 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+import xgboost
+import pickle
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
 
 
-def save_obj(file_path,obj):
+def save_obj(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
-        os.makedirs(dir_path,exist_ok=True)
-        
-        with open(file_path,"wb") as file_obj:
-            dill.dump(obj,file_obj)
-            
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models,):
+    try:
+        report = {}
+        
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            
+            model.fit(X_train,y_train)
+            y_train_pred = model.predict(X_train)
+            
+            y_test_pred = model.predict(X_test)
+            
+            train_model_score = r2_score(y_train,y_train_pred)
+            test_model_Score = r2_score(y_test,y_test_pred)
+            
+            report[list(models.keys())[i]] = test_model_Score
+    
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
